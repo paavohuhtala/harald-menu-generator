7abd56d2b991f52ea8127c3223284647f1c6fe95
paavohuhtala <paavo.huhtala@gmail.com>
2019-03-30 22:07:00 +0200

    Add more words and utils

--- src/pohina.js
+++ src/pohina.js
@@ -1,28 +1,69 @@
 import { maybe, oneOf, evaluate } from "./dsl"
-import { nth, upperFirst } from "lodash/fp"
-import { wordList, prependAll } from "./util";
+import { upperFirst, random } from "lodash/fp"
+import { wordList, prependAll, nOf, natListFi } from "./util";
 
-const [positiveAdjective, positiveAdjectivePossessive, positiveAdjectivePluralPosessive] = wordList([
-  ["kestävä", "kestävän", "kestävien"],
-  ["skaalautuva", "skaalautuvan", "skaalautuvien"],
-  ["innovatiivinen", "innovatiivisen", "innovatiivisten"]
+const [positiveAdjective, positiveAdjectivePlural, positiveAdjectivePossessive, positiveAdjectivePluralPosessive, positiveAdverb] = wordList([
+  ["kestävä", "kestävät", "kestävän", "kestävien", "kestävästi"],
+  ["skaalautuva", "skaalautuvat", "skaalautuvan", "skaalautuvien", "skaalautuvasti"],
+  ["innovatiivinen", "innovatiiviset", "innovatiivisen", "innovatiivisten", "innovatiivisesti"]
+  ["ketterä", "ketterät", "ketterän", "ketterien", "ketterästi"],
+  ["ketterä", "ketterät", "ketterän", "ketterien", "ketterästi"],
+  ["avoin", "avoimet", "avoimen", "avoimien", "avoimesti"],
 ])
 
 const subjectPrefix = oneOf(
   "yritys",
   "talous",
-  "rajapinta", 
+  "rajapinta-", 
   "pilvi",
-  "tietokanta",
+  "tietokanta-",
   "johtamis",
   "tekoäly",
-  "palvelu"
+  "palvelu",
+  "XML-",
+  "REST-",
+  "API-",
+  "GraphQL-",
+  "alusta-",
+  "kokonais",
+  "lohkoketju",
+  "mikropalvelu",
+  "nanopalvelu"
 )
 
-const [subject, subjectPossessive, subjectPluralPosessive] = wordList([
-  prependAll(oneOf("API-", "rajapinta"), ["talous", "talouden", "talouksien"]),
-  prependAll(maybe(subjectPrefix), ["ekosysteemi", "ekosysteemin", "ekosysteemien"]),
-  prependAll(maybe(subjectPrefix), ["arkkitehtuuri", "arkkitehtuurin", "arkkitehtuurien"])
+const [subject, subjectPlural, subjectPossessive, subjectPluralPosessive, toSubject] = wordList([
+  prependAll(oneOf("API-", "rajapinta"), ["talous", "taloudet", "talouden", "talouksien", "talouteen"]),
+  prependAll(maybe(subjectPrefix), ["ekosysteemi", "ekosysteemit", "ekosysteemin", "ekosysteemien", "ekosysteemiin"]),
+  prependAll(maybe(subjectPrefix), ["arkkitehtuuri", "arkkitehtuurit", "arkkitehtuurin", "arkkitehtuurien", "arkkitehtuuriin"])
+])
+
+const buzzword = [
+  "tekoäly",
+  "koneoppiminen",
+  "big data",
+  "integraatiot",
+  "rajapinnat",
+  "alustatalous",
+  "lohkoketjut",
+  "Ethereum",
+  "Bitcoin",
+  "Watson",
+  "JIRA",
+  "AWS",
+  "DB2",
+  "serverless",
+  [oneOf("API", "dev", "AI", "ML", "XML", "JSON", "REST", "SQL", "DB", "SOAP", "SAFE", "ledger", "coin"), "ops"]
+]
+
+const buzzwords = () => natListFi(nOf(random(1, 3), buzzword))
+
+const [verb] = wordList([
+  ["integroiminen"],
+  ["kehittäminen"],
+  ["rakentaminen"],
+  ["ulkoistaminen"],
+  ["ketteröittäminen"],
+  ["kiihdyttäminen"]
 ])
 
 const growth = oneOf(
@@ -31,9 +72,35 @@ const growth = oneOf(
   "kehitys"
 )
 
+const time = oneOf(
+  "vuonna 2019",
+  "vuonna 2020",
+  "2010-luvulla",
+  "2020-luvulla",
+  "2030-luvulla",
+)
+
+const rhetoricalQuestion = oneOf(
+  "uhka vai mahdollisuus?",
+  "miten aloitan?",
+  [subjectPluralPosessive, " tulevaisuus?"],
+  "miksi?"
+)
+
 const pattern = oneOf(
-  [positiveAdjectivePluralPosessive, " ", subjectPluralPosessive],
-  [positiveAdjectivePossessive, " ", subjectPossessive]
+  [positiveAdjectivePluralPosessive, " ", subjectPluralPosessive, " ", positiveAdjective, " ", verb, maybe([" ", time])],
+  [positiveAdjectivePossessive, " ", subjectPossessive, " ", positiveAdjective, " ", verb],
+  [subjectPossessive, " monetisaatio"],
+  [buzzwords, " -  ", rhetoricalQuestion],
+  [oneOf(oneOf(...buzzword), subject), " ", oneOf("organisaatiossa", "valtiohallinnossa", "henkilöstönhallinassa")],
+  [maybe([positiveAdjective, " "]), subject, " liiketoiminnan tukena"],
+  [positiveAdjectivePlural, " ", subjectPlural, oneOf(
+    [" ", time],
+    [" - ", positiveAdverb],
+    [" - ", rhetoricalQuestion],
+    [" - avain ", toSubject]
+  )],
+  [buzzwords, " ", time]
 )
 
 export const generatePresentation = () => upperFirst(evaluate(pattern))
--- src/util.js
+++ src/util.js
@@ -1,7 +1,31 @@
-import { oneOf } from "./dsl"
-import { unzip, reject, eq } from "lodash/fp"
+import { oneOf, evaluate } from "./dsl"
+import { unzip, reject, eq, shuffle } from "lodash/fp"
 
 export const __ = Symbol("placeholder")
 
 export const prependAll = (pre, arr) => arr.map(x => x == __ ? __ : [pre, x])
 export const wordList = ls => unzip(ls).map(words => oneOf(...reject(eq(__), words)))
+
+export const sepBy = (x, arr) => () => arr.join(x)
+export const nOf = (n, arr) => console.log(shuffle(arr).slice(0, n)) || shuffle(arr).slice(0, n)
+
+export const natList = (sep, last) => arr => {
+  const parts = arr.map(evaluate)
+
+  if (parts.length === 0) {
+    return ""
+  }
+
+  if (parts.length === 1) {
+    return parts[0]
+  }
+
+  if (parts.length === 2) {
+    const [a, b] = parts
+    return `${a}${last}${b}`
+  }
+
+  return `${parts.slice(0, parts.length - 1).join(sep)}${last}${parts[parts.length - 1]}`
+}
+
+export const natListFi = natList(", ", " ja ")
